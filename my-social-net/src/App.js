d66d041c742f7b42fd1362851c0fec285052a5b2
Shlkirill <kirill_94_@mail.ru>
2020-07-20 21:31:19 +0300

    UPD

--- my-social-net/src/App.js
+++ my-social-net/src/App.js
@@ -7,6 +7,7 @@ function SocialNetworkSite() {
     <div className='SNsite'>
       <Header />
       <LeftMenu />
+      <MainBlock />
     </div>
   );
 }
@@ -15,12 +16,9 @@ function SocialNetworkSite() {
 function Header() {
   return (
     <div className='header'>
-      <div className='header__logo'>
-        <img src='http://www.pngall.com/wp-content/uploads/2016/06/Superman-Logo-Free-Download-PNG.png'/>
-      </div>
-      <div className='header__button'>
-        <button>Выход</button>
-      </div>
+        <img src='http://www.pngall.com/wp-content/uploads/2016/06/Superman-Logo-Free-Download-PNG.png' className='header__logo'/>
+        <p>SOCIAL CITE</p>
+        <button className='header__button'>Выход</button>
     </div>
   )
 }
@@ -34,6 +32,35 @@ function LeftMenu() {
       <li>Music</li>
       <li>Settings</li>
     </ul>
+  )
+}
+
+function MainBlock() {
+  return(
+    <div className='main'>
+      <img src='http://dgdesign.ru/uploads/posts/2018-05/1525700405_shapka-sayta-tehnologii-2114654127851.jpg' className='main__topImg' />
+      <div className='user'>
+          <img src='https://clraik.com/forum/customavatars/avatar14341_15.gif' className='user__avatar'/>
+        <div className='user__info'>
+          <h4>Kirill S.H</h4>
+          <p><span>Date of Birth: </span>12.08.1994 g.</p>
+          <p><span>City: </span>Belogorod</p>
+          <p><span>Education: </span>Belgorod economic university</p>
+          <p><span>tel.: </span>8-800-555-35-35</p>
+        </div>
+      </div>  
+        <div className='side'>
+          <div className='side__newMessage'>
+            <h3> My posts</h3>
+            <input type='text' className='side__text'/>
+            <input type='submit' className='side__submit'/>
+          </div>
+          <div className='side__allMessages'>
+              <img src='https://clraik.com/forum/customavatars/avatar14341_15.gif' className='side__avatar'/>
+              <p>Hello all! How are you?</p>
+          </div>
+        </div>
+    </div>
   )
 }
 export default SocialNetworkSite;
