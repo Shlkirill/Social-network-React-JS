1dab89a4cef0ab6408b36fa7a130487b9b94d7a2
Kirill <kirill_94_@mail.ru>
2020-07-20 17:01:40 +0300

    UPD

--- my-social-net/src/App.js
+++ my-social-net/src/App.js
@@ -4,10 +4,36 @@ import './App.css';
 
 function SocialNetworkSite() {
   return (
-    <div>
-      <h3>Hello</h3>
+    <div className='SNsite'>
+      <Header />
+      <LeftMenu />
     </div>
   );
 }
 
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='header__logo'>
+        <img src='http://www.pngall.com/wp-content/uploads/2016/06/Superman-Logo-Free-Download-PNG.png'/>
+      </div>
+      <div className='header__button'>
+        <button>Выход</button>
+      </div>
+    </div>
+  )
+}
+
+function LeftMenu() {
+  return (
+    <ul className='menu'>
+      <li>Profile</li>
+      <li>Messages</li>
+      <li>News</li>
+      <li>Music</li>
+      <li>Settings</li>
+    </ul>
+  )
+}
 export default SocialNetworkSite;
