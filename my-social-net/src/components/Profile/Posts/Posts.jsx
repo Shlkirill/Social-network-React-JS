c684e1c39c0c2b0dbc19cc4c49fe12384edaedff
Kirill <kirill_94_@mail.ru>
2020-07-28 17:20:39 +0300

    Map

--- my-social-net/src/components/Profile/Posts/Posts.jsx
+++ my-social-net/src/components/Profile/Posts/Posts.jsx
@@ -3,23 +3,23 @@ import styles from './Posts.module.css';
 import Post from './Post/Post';
 
 const Posts = () => {
+  
   let postsData = [
     {id: 1, text: 'Hello! How are you?', likes: 3 },
     {id: 2, text: 'My name is Kirill!', likes: 8 }
   ]
 
-
+  let result = postsData.map(item => <Post text={item.text} likes={item.likes}/>);
 
   return( 
     <div className={styles.side}>
       <div className={styles.createPost}>
         <h3> My posts</h3>
-        <input type='text' className={styles.inputText}/>
-        <input type='submit' className={styles.inputSubmit}/>
+        <textarea className={styles.inputText} placeholder='How are you?'></textarea>
+        <button className={styles.inputSubmit}>Send</button>
       </div>
       <div className={styles.posts}>
-          <Post text={postsData[0].text} likes={postsData[0].likes}/>
-          <Post text={postsData[1].text} likes={postsData[1].likes}/>
+          {result}
       </div>
     </div>
   )
